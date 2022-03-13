9d953066ec1f6dac006223bab825594c46f4b0be
Olle Jernström WSL <ollejer@ug.kth.se>
2022-03-13 22:46:53 +0100

    Make Google login work with auth context manager

--- hooks/useAuth.tsx
+++ hooks/useAuth.tsx
@@ -1,19 +1,66 @@
 import { View, Text } from 'react-native';
 import React, { createContext, useContext, useState } from 'react';
+import * as Google from 'expo-auth-session/providers/google';
+import * as WebBrowser from 'expo-web-browser';
 
 
+interface AuthContextType {
+    user?: string;
+    accessToken?: string | undefined;
+}
 
-
-const AuthContext = createContext({
-    user: '',
+const AuthContext: React.Context<{
+    user: any,
+    getUser: any,
+    accessToken: string,
+    promptAsync: any
+}> = createContext({
+    user: null,
+    getUser: null,
+    accessToken: '',
+    promptAsync: null,
 });
 
 
 
 export const AuthProvider: React.FC = ({ children }) => {
+    const [accessToken, setAccessToken] = React.useState<string | undefined>();
+    const [userInfo, setUserInfo] = React.useState();
+    const [message, setMessage] = React.useState<any>();
+
+    const [request, response, promptAsync] = Google.useAuthRequest({
+        androidClientId: '1029181124854-4525710ob94m95h69ac6vqosd004dic9.apps.googleusercontent.com',
+        iosClientId: '1029181124854-lsnpj2grnerbges0jio1rbj2ggo29o41.apps.googleusercontent.com',
+        expoClientId: '1029181124854-4vfhtis83ajcdns1nlauac7jg10drs4p.apps.googleusercontent.com',
+        webClientId: '1029181124854-4525710ob94m95h69ac6vqosd004dic9.apps.googleusercontent.com'
+    });
+
+    React.useEffect(() => {
+        console.log(0);
+        setMessage(JSON.stringify(response));
+        if (response?.type === 'success') {
+            // const ensureToken = response.authentication ? response.authentication?.accessToken : '';
+            setAccessToken(response.authentication?.accessToken);
+        }
+    }, [response]);
+
+    async function getUserData() {
+        const userInfoResponse = await fetch('https://www.googleapis.com/userinfo/v2/me', {
+            headers: { Authorization: `Bearer ${accessToken}`}
+        });
+    
+        userInfoResponse.json().then(data => {
+            setUserInfo(data);
+        });
+    }
     return (
         <AuthContext.Provider
-            value={{user: ''}}>
+            value={{
+                user: userInfo,
+                getUser: getUserData,
+                accessToken: accessToken ? accessToken : '',
+                promptAsync,
+            }}>
             {children}
         </AuthContext.Provider>
     );
--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -15,42 +15,16 @@ const config = {
 };
 
 const LoginScreen = () => {
-    const [accessToken, setAccessToken] = React.useState<string | undefined>();
-    const [userInfo, setUserInfo] = React.useState<any>();
-    const [message, setMessage] = React.useState<any>();
-
-    const [request, response, promptAsync] = Google.useAuthRequest({
-        androidClientId: '1029181124854-4525710ob94m95h69ac6vqosd004dic9.apps.googleusercontent.com',
-        iosClientId: '1029181124854-lsnpj2grnerbges0jio1rbj2ggo29o41.apps.googleusercontent.com',
-        expoClientId: '1029181124854-4vfhtis83ajcdns1nlauac7jg10drs4p.apps.googleusercontent.com',
-        webClientId: '1029181124854-4525710ob94m95h69ac6vqosd004dic9.apps.googleusercontent.com'
-    });
-
-    React.useEffect(() => {
-        console.log(0);
-        setMessage(JSON.stringify(response));
-        if (response?.type === 'success') {
-            setAccessToken(response.authentication?.accessToken);
-        }
-    }, [response]);
-
-    async function getUserData() {
-        const userInfoResponse = await fetch('https://www.googleapis.com/userinfo/v2/me', {
-            headers: { Authorization: `Bearer ${accessToken}`}
-        });
+    const { user, accessToken, getUser, promptAsync } = useAuth();
     
-        userInfoResponse.json().then(data => {
-            setUserInfo(data);
-        });
-    }
 
     function showUserInfo() {
-        if (userInfo) {
+        if (user) {
             return (
                 <View>
-                    <Image source={{uri: userInfo.picture}} style={styles.profilePic} />
-                    <Text>Welcome {userInfo.name}</Text>
-                    <Text>{userInfo.email}</Text>
+                    <Image source={{uri: user.picture}} style={styles.profilePic} />
+                    <Text>Welcome {user.name}</Text>
+                    <Text>{user.email}</Text>
                 </View>
             );
         }
@@ -62,7 +36,7 @@ const LoginScreen = () => {
             {showUserInfo()}
             <Button 
                 title={accessToken ? 'Get User Data' : 'Login'}
-                onPress={accessToken ? getUserData : () => { promptAsync({useProxy: false, showInRecents: true}); }}
+                onPress={accessToken ? getUser : () => { promptAsync({useProxy: false, showInRecents: true}); }}
             />
             <StatusBar/>
         </View>
