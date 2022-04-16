d5c42aefa358e30accc1a10067384d6da0dd2992
Olle Jernström WSL <ollejer@ug.kth.se>
2022-04-16 15:58:32 +0200

    fix: fix lasting merge conflicts

--- App.tsx
+++ App.tsx
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
-import { t, color } from 'react-native-tailwindcss';
-import StackNavigator from './StackNavigator';
-import { NavigationContainer } from '@react-navigation/native';
-import { AuthProvider } from './hooks/useAuth';
-
-export default function App() {
-    return (
-        <NavigationContainer>
-            <AuthProvider>
-                <StackNavigator />
-            </AuthProvider>
-        </NavigationContainer>
-    );
-}
-=======
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
@@ -131,4 +112,3 @@ const App = () => {
 
 
 export default App;
->>>>>>> master
