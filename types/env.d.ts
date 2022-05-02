40a59774e10167350d69bdd03f7205f53b15280d
Olle Jernström <ollejer@ug.kth.se>
2022-05-02 12:12:36 +0200

    refactor: move redirect uris to environment

--- types/env.d.ts
+++ types/env.d.ts
@@ -1,4 +1,6 @@
 declare module '@env' {
     export const CLIENT_ID: string;
     export const CLIENT_SECRET: string;
+    export const REDIRECT_URI: string;
+    export const REDIRECT_URI_WEB: string;
   }
