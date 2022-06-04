8fec4b8c3381fe4fd7702074d761837d59315e82
Olle Jernström WSL <ollejer@ug.kth.se>
2022-06-04 12:49:56 +0200

    wip

--- hooks/utils/auth-utils.ts
+++ hooks/utils/auth-utils.ts
@@ -3,7 +3,7 @@ import { REDIRECT_URI, REDIRECT_URI_WEB } from '@env';
 
 // TODO: can maybe be replaced with: getRedirectUrl from expo auth session
 const redirectUri = Platform.OS === 'web' ? REDIRECT_URI_WEB : REDIRECT_URI;
-
+console.log(REDIRECT_URI, REDIRECT_URI_WEB);
 const authorizationEndpoint = 'https://accounts.spotify.com/authorize';
 const tokenEndpoint = 'https://accounts.spotify.com/api/token';
 const meEndpoint = 'https://api.spotify.com/v1/me';
