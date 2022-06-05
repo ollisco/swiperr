0b0eecfa492e9c8c315ce9af522acfa697841db6
Olle Jernström <ollejer@ug.kth.se>
2022-06-05 10:13:48 +0200

    refactor: Add assets (css) to lint and lint:fix

--- assets/data/dummy_data_playlists.ts
+++ assets/data/dummy_data_playlists.ts
@@ -1,38 +1,33 @@
-
-
-
-
-
 export const dummyDataPlaylists = [
-    {
-        id: '1',
-        name: 'Playlist 1',
-        image: require('../../assets/images/logic.jpg'),
-    },
-    {
-        id: '2',
-        name: 'Playlist 2',
-        image: require('../../assets/images/odz.jpg'),
-    },
-    {
-        id: '3',
-        name: 'Playlist 3',
-        image: require('../../assets/images/metallica.jpg'),
-    },
-    // make 5 more object like that
-    {
-        id: '4',
-        name: 'Playlist 4',
-        image: require('../../assets/images/dio.jpg'),
-    },
-    {
-        id: '5',
-        name: 'Playlist 5',
-        image: require('../../assets/images/post.jpg'),
-    },
-    {
-        id: '6',
-        name: 'Playlist 6',
-        image: require('../../assets/images/skepta.jpg'),
-    }
+  {
+    id: '1',
+    name: 'Playlist 1',
+    image: require('../images/logic.jpg'),
+  },
+  {
+    id: '2',
+    name: 'Playlist 2',
+    image: require('../images/odz.jpg'),
+  },
+  {
+    id: '3',
+    name: 'Playlist 3',
+    image: require('../images/metallica.jpg'),
+  },
+  // make 5 more object like that
+  {
+    id: '4',
+    name: 'Playlist 4',
+    image: require('../images/dio.jpg'),
+  },
+  {
+    id: '5',
+    name: 'Playlist 5',
+    image: require('../images/post.jpg'),
+  },
+  {
+    id: '6',
+    name: 'Playlist 6',
+    image: require('../images/skepta.jpg'),
+  },
 ];
--- components/card-components/CardStackHandler.tsx
+++ components/card-components/CardStackHandler.tsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import { View } from 'react-native';
 import CardStack, { Card } from 'react-native-card-stack-swiper';
 import DATA from '../../assets/data/dummy_data_songs';
-import { DARK_GRAY, WHITE } from '../../assets/styles';
+import { DARK_GRAY } from '../../assets/styles';
 import useSpotifyContext from '../../hooks/useSpotifyAuth';
 import { SwipedCardContextT } from '../../types';
 import CardItem from './CardItem';
