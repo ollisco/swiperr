3eb79257bf4bf8dbedb8fe124fcea1144234f11f
Olle Jernström <56885633+ollisco@users.noreply.github.com>
2022-06-18 20:24:59 +0200

    Improve redirect (#69)
    * [refactor] improve css
    
    * [lint:fix]
    
    * [feat] improve redirectUri autodetect
    
    * [refactor] cleanup
    
    Co-authored-by: Olle Jernström <ollejer@ug.kth.se>

--- hooks/utils/auth-utils.ts
+++ hooks/utils/auth-utils.ts
@@ -2,12 +2,12 @@ import { Platform } from 'react-native';
 import { REDIRECT_URI, REDIRECT_URI_WEB } from '@env';
 
 // TODO: can maybe be replaced with: getRedirectUrl from expo auth session
-const redirectUriWeb = ['http://localhost:19006/', 'https://ollisco.github.io/swiperr/'].includes(window.location.href)
+const devRedirect = 'http://localhost:19006/';
+const deployRedirect = 'https://ollisco.github.io/swiperr/';
+const redirectUriWeb = [devRedirect, deployRedirect].includes(window.location.href)
   ? window.location.href : REDIRECT_URI_WEB;
+
 const redirectUri = Platform.OS === 'web' ? redirectUriWeb : REDIRECT_URI;
-
-
-
 console.log(redirectUri, window.location.href);
 // TODO: add all endpoints to here instead of useSpotifyAuth
 const authorizationEndpoint = 'https://accounts.spotify.com/authorize';
