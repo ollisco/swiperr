e1bb186b5025ebee0fb575d4614f724321aef560
ollisco <56885633+ollisco@users.noreply.github.com>
2022-07-10 17:31:12 +0000

    Deploying to gh-pages from @ ollisco/swiperr@7df9c5c9b5f5eba8b22ea71da260393cf1375d3d 🚀

---
+++ components/ProfileItem.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import Icon from './Icon';
+import { ProfileItemT } from '../types';
+import styles, { WHITE } from '../assets/styles';
+
+function ProfileItem({
+  followerCount,
+  location,
+  name,
+  topTracks,
+  topArtists,
+  topGenres,
+}: ProfileItemT) {
+  return (
+    <View style={styles.containerProfileItem}>
+
+      <Text style={styles.name}>{name}</Text>
+
+      <Text style={styles.descriptionProfileItem}>
+        {followerCount}
+        {' followers '}
+        -
+        {' '}
+        {location}
+      </Text>
+
+      <View style={styles.info}>
+        <Text style={styles.iconProfile}>
+          <Icon name="person" size={12} color={WHITE} />
+        </Text>
+        <Text style={styles.infoContent}>{topArtists}</Text>
+      </View>
+
+      <View style={styles.info}>
+        <Text style={styles.iconProfile}>
+          <Icon name="musical-notes" size={12} color={WHITE} />
+        </Text>
+        <Text style={styles.infoContent}>{topTracks}</Text>
+      </View>
+
+      <View style={styles.info}>
+        <Text style={styles.iconProfile}>
+          <Icon name="albums-outline" size={12} color={WHITE} />
+        </Text>
+        <Text style={styles.infoContent}>{topGenres}</Text>
+      </View>
+
+      <View style={styles.info}>
+        <Text style={styles.iconProfile}>
+          <Icon name="location" size={12} color={WHITE} />
+        </Text>
+        <Text style={styles.infoContent}>{location}</Text>
+      </View>
+
+    </View>
+  );
+}
+
+export default ProfileItem;
---
+++ types.ts
@@ -0,0 +1,131 @@
+import CardStack from "react-native-card-stack-swiper";
+import Swiper from "react-native-deck-swiper";
+
+export type CardItemT = {
+  description?: string;
+  hasActions?: boolean;
+  hasVariant?: boolean;
+  image: any;
+  artist: string;
+  popularity?: string;
+  track: string;
+  previousVolume?: number;
+  setVolume?: (val: number) => void;
+  color?: string;
+  id: number,
+  releaseDate?: string;
+};
+
+export type CardItemRowT = {
+  name: string;
+  image: any;
+}
+
+
+export type SwipedCardContextT = {
+  volume: number;
+  updateVolume: (value: number) => void;
+  rgb: string;
+  setRGB: (value: string) => void;
+  pressedTrack: any;
+  setPressedTrack: (value: any) => void;
+  showPlaylists: boolean;
+  setShowPlaylists: (value: boolean) => void;
+  swiper: Swiper<DataSong> | null;
+  setSwiper: (value: Swiper<DataSong> | null) => void;
+  showType: string;
+  setShowType: (value: string) => void;
+  recommendedIndex: number;
+  setRecommendedIndex: (value: number) => void;
+  newReleasesIndex: number;
+  setNewReleasesIndex: (value: number) => void;
+  allowVolumeControll: boolean;
+}
+
+
+export type IconT = {
+  name: any;
+  size: number;
+  color: string;
+  style?: any;
+  onPress?: (e: any) => void;
+  useFontAwesome?: boolean;
+};
+
+export type MessageT = {
+  image: any;
+  lastMessage: string;
+  name: string;
+};
+
+export type ProfileItemT = {
+  followerCount?: string;
+  topTracks?: string;
+  topArtists?: string;
+  topGenres?: string;
+  location?: string;
+  name: string;
+};
+
+export type TabBarIconT = {
+  focused: boolean;
+  iconName: any;
+  text: string;
+};
+
+export type DataSong = {
+  id: number;
+  track: string;
+  match: string;
+  artist: string;
+  album: string;
+  image: any;
+  location?: string;  
+};
+
+
+export type DataProfile = {
+  id: number;
+  name: string;
+  isOnline: boolean;
+  match: string; // . How similiar is this person to you musically?
+  description: string;
+  message: string;
+  image: any;
+  age?: string;
+  infoPersonal?: string;
+  infoMusic?: string;
+  lastSeen?: string;
+  location?: string;
+  topTracks?: string;
+  topArtists?: string;
+  topGenres?: string;
+};
+
+export type SettingItemSwitchT = {
+  text: string;
+  onValueChange: (value: boolean) => void;
+  value: boolean;
+  icon?: any;
+};
+
+export type SettingItemDropdownT = {
+  header: string;
+  explanation?: string;
+  options: string[];
+  defaultValue?: string;
+  onSelect?: (value: any) => void;
+  dropdownSize?: dropdownSize;
+}
+
+export enum dropdownSize {
+  SMALL = "small",
+  MEDIUM = "medium",
+  LARGE = "large",
+}
+
+export enum DeviceType {
+  COMPUTER = "Computer",
+  SMARTPHONE = "Smartphone",
+  SPEAKER = "speaker",
+}
