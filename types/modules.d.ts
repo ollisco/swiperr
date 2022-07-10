4660fb536cefb72dfc1a3bcaf2e13293bbfab90b
gh actions <gh-actions@swiper.com>
2022-07-10 18:36:20 +0000

    Updates

--- types/modules.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'react-native-spotify-web-api';
-declare module 'js-cookie';
-declare module 'react-spotify-auth';
-declare module 'react-spotify-api';
-declare module 'country-data';
-declare module 'react-native-slider';
-declare module 'react-native-modal-dropdown';
