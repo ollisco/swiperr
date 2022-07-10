af393fc244037c36f64fedf9e0dc4568f8b0595c
gh actions <gh-actions@swiper.com>
2022-07-10 18:47:07 +0000

    Updates

--- screens/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Explore } from './Explore';
-export { default as Liked } from './Liked';
-export { default as Messages } from './Messages';
-export { default as Profile } from './Profile';
-export { default as LoginPage } from './LoginPage';
