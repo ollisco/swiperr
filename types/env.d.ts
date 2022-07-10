cf00121049d021ed84b854e34c694f6f8c2c6e9c
gh actions <gh-actions@swiper.com>
2022-07-10 17:58:12 +0000

    Updates

--- types/env.d.ts
+++
@@ -1,6 +0,0 @@
-declare module '@env' {
-    export const CLIENT_ID: string;
-    export const CLIENT_SECRET: string;
-    export const REDIRECT_URI: string;
-    export const REDIRECT_URI_WEB: string;
-  }
