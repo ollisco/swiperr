4b7d9c8c9bb3f1b1f20f1c7ed2cb1f05063c24cf
ollisco <56885633+ollisco@users.noreply.github.com>
2022-07-10 17:41:35 +0000

    Deploying to gh-pages from @ ollisco/swiperr@95e8731b9933efa89f91b6b04ee47560945f2d53 🚀

---
+++ components/card-components/Cards.tsx
@@ -0,0 +1,69 @@
+import { View, Text, TouchableOpacity } from 'react-native';
+import React, { useContext } from 'react';
+import styles from '../../assets/styles';
+import Settings from '../Settings';
+import Filters from '../Filters';
+import Login from '../Login';
+import useSpotifyContext from '../../hooks/useSpotifyAuth';
+import CardStackHandler from './CardStackHandler';
+import { SwipeCardContext } from './CardProvider';
+import { SwipedCardContextT } from '../../types';
+
+type Props = {}
+
+function Cards(props: Props) {
+  const {
+    user, queueAndSkip, recommendedTracks: userRecommendedTracks, newReleases,
+
+  } = useSpotifyContext();
+  const {
+    showType, setShowType, recommendedIndex, newReleasesIndex,
+  } = useContext(SwipeCardContext) as SwipedCardContextT;
+
+  const recommendedActiveStyle = showType === 'recommended' ? styles.exploreTopCenterTextActive : {};
+  const newActiveStyle = showType === 'new' ? styles.exploreTopCenterTextActive : {};
+
+  return (
+    <View style={styles.containerHome}>
+      <View style={styles.top}>
+        {/* Testing spotify login in settings component */}
+        <Settings />
+        {user ? (
+          <View style={styles.exploreTopCenter}>
+            <TouchableOpacity onPress={() => {
+              if (showType !== 'recommended' && userRecommendedTracks) {
+                setShowType('recommended');
+                queueAndSkip(userRecommendedTracks[recommendedIndex].uri);
+              }
+            }}
+            >
+              <Text style={[styles.exploreTopCenterText, recommendedActiveStyle]}>
+                Recommended-2
+              </Text>
+            </TouchableOpacity>
+            <Text style={styles.exploreTopCenterText}>
+              |
+            </Text>
+            <TouchableOpacity onPress={() => {
+              if (showType !== 'new' && newReleases) {
+                setShowType('new');
+                queueAndSkip(newReleases[newReleasesIndex].uri);
+              }
+            }}
+            >
+              <Text style={[styles.exploreTopCenterText, newActiveStyle]}>
+                New Releases
+              </Text>
+            </TouchableOpacity>
+          </View>
+        ) : (
+          <Login />
+        )}
+        <Filters />
+      </View>
+      <CardStackHandler />
+    </View>
+  );
+}
+
+export default Cards;
