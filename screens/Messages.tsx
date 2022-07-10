fb20e27a66f2e93a210387fcc20b02675a6172c8
ollisco <56885633+ollisco@users.noreply.github.com>
2022-07-10 17:57:19 +0000

    Deploying to gh-pages from @ ollisco/swiperr@a0acdd98b587e330943bcddd7199a7108b38bb92 🚀

---
+++ screens/Messages.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import {
+  Text,
+  TouchableOpacity,
+  ImageBackground,
+  View,
+  FlatList,
+} from 'react-native';
+import { Icon, Message } from '../components';
+import DEMO from '../assets/data/dummy_data_profiles';
+import styles, { GRAY } from '../assets/styles';
+import BG_IMAGE from '../assets/images/bg2.jpg';
+
+function Messages() {
+  return (
+    <View>
+      <ImageBackground
+        source={BG_IMAGE}
+        style={styles.bg}
+      >
+        <View style={styles.containerMessages}>
+          <View style={styles.top}>
+            <Text style={styles.title}>Messages</Text>
+            <TouchableOpacity>
+              <Icon name="person-add" color={GRAY} size={20} style={styles.topIconRight} />
+            </TouchableOpacity>
+          </View>
+          <FlatList
+            data={DEMO}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={({ item }) => (
+              <TouchableOpacity>
+                <Message
+                  image={item.image}
+                  name={item.name}
+                  lastMessage={item.message}
+                />
+              </TouchableOpacity>
+            )}
+          />
+        </View>
+      </ImageBackground>
+    </View>
+  );
+}
+
+export default Messages;
